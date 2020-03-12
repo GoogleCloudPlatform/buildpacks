4b94f6980688b829fbaeaafabbabcea47eb0c52c
Lukas Berger <bergerl@google.com>
2020-03-12 10:22:29 -0700

    Remove google3-specific comments from acceptance tests
    PiperOrigin-RevId: 300574138
    Change-Id: I89f5b0bc2319afc60e490069ed8857a1d780baf0

--- builders/gae/java11/acceptance/acceptance_test.go
+++ builders/gae/java11/acceptance/acceptance_test.go
@@ -11,11 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// TODO: Add an equivalent script to the open-source version.
-// Run these tests using:
-//   apphosting/runtime/titanium/buildpacks/tools/run-acceptance-tests.sh --runtime=gaejava11
-//
 package acceptance
 
 import (
--- builders/gae/php73/acceptance/acceptance_test.go
+++ builders/gae/php73/acceptance/acceptance_test.go
@@ -11,10 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Run these tests using:
-//   apphosting/runtime/titanium/buildpacks/tools/run-acceptance-tests.sh --runtime=gaephp73
-//
 package acceptance
 
 import (
