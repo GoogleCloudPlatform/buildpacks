27f1b596d75c822db8880f8e3c1a5db369ca66a7
Brian de Alwis <bdealwis@google.com>
2020-03-30 14:33:29 -0700

    Extract licenses for third-party Go libraries and place in GCP builder images.
    PiperOrigin-RevId: 303824694
    Change-Id: Ia0fb997273d80d5d985f7e7d3c43b4109cf6bb92

--- builders/gcp/base/stack/build.sh
+++ builders/gcp/base/stack/build.sh
@@ -13,8 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -euo pipefail
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Gather license files in a tar as Docker's ADD preserves file ownership and mod times.
+# The tar must be located within the docker build context (${DIR})
+echo "> Gathering licenses"
+LICENSES_BASE="licenses"
+LICENSES_TAR="licenses.tar"
+trap "rm -rf \"${DIR}/${LICENSES_TAR}\"" EXIT
+tar -cf "${DIR}/${LICENSES_TAR}" --mode="a=rX" --mtime="@0" --owner=0 --group=0 --sort=name -C "${DIR}/${LICENSES_BASE}" .
+tar -rf "${DIR}/${LICENSES_TAR}" --mode="a=rX" --mtime="@0" --owner=0 --group=0 -C "${DIR}" licenses.yaml
+
+echo "> Building gcp/base common image"
 docker build -t "common" -f "${DIR}/Dockerfile.parent" "${DIR}"
-docker build --build-arg "from_image=common" -t "gcr.io/buildpacks/gcp/build" -f "${DIR}/Dockerfile.build" "${DIR}"
+echo "> Building gcr.io/buildpacks/gcp/run"
 docker build --build-arg "from_image=common" -t "gcr.io/buildpacks/gcp/run" -f "${DIR}/Dockerfile.run" "${DIR}"
+echo "> Building gcr.io/buildpacks/gcp/build"
+docker build --build-arg "from_image=common" -t "gcr.io/buildpacks/gcp/build" -f "${DIR}/Dockerfile.build" "${DIR}"
+
+echo "> Validating gcr.io/buildpacks/gcp/build"
+container-structure-test test -c build-test.yaml -i gcr.io/buildpacks/gcp/build:latest
