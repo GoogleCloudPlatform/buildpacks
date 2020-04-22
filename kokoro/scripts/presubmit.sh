b8daa51bef0a8247d58d28a0c94300b1c465c562
Lukas Berger <bergerl@google.com>
2020-04-21 17:52:57 -0700

    Add presubmit build configurations for GAE and GCF builders
    PiperOrigin-RevId: 307717667
    Change-Id: I2ab6984d15a1badc2d0b55a38da589c2c33d33b2

--- kokoro/scripts/presubmit.sh
+++ kokoro/scripts/presubmit.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-# The run_acceptance_tests.sh script runs acceptance tests for $PRODUCT.
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# The presubmit.sh script runs acceptance tests for tests matching $FILTER.
+#
+# The value of FILTER is passed to Blaze query and used to select a subset of
+# targets. Tests are split into smaller sets that can run concurrently on
+# different machines.
 
 set -euo pipefail
 
