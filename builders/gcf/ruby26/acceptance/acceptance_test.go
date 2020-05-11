8d825c2416736ec3ef7e26d9de677ed53ab9eb42
Daniel Azuma <dazuma@google.com>
2020-05-11 13:37:42 -0700

    Add functions-framework buildpack and acceptance tests for ruby26
    PiperOrigin-RevId: 310982684
    Change-Id: Ideaaa5d4a4e01c9290755944a2503a4d28432dc1

---
+++ builders/gcf/ruby26/acceptance/acceptance_test.go
@@ -0,0 +1,86 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/buildpacks/pkg/acceptance"
+)
+
+func init() {
+	acceptance.DefineFlags()
+}
+
+func TestAcceptance(t *testing.T) {
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.Test{
+		{
+			Name: "function with dependencies",
+			App:  "with_dependencies",
+		},
+		{
+			Name:   "function with runtime env var",
+			App:    "with_env_var",
+			RunEnv: []string{"FOO=foo"},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.Path = "/testFunction"
+			tc.Env = append(tc.Env,
+				"GOOGLE_FUNCTION_TARGET=testFunction",
+				"GOOGLE_RUNTIME=ruby26",
+			)
+
+			acceptance.TestApp(t, builder, tc)
+		})
+	}
+}
+
+func TestFailures(t *testing.T) {
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.FailureTest{
+		{
+			App:       "fail_ruby_version",
+			MustMatch: "but your Gemfile specified",
+		},
+		{
+			App:       "fail_framework_missing",
+			MustMatch: "unable to execute functions-framework",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.App, func(t *testing.T) {
+			t.Parallel()
+
+			tc.Env = append(tc.Env,
+				"GOOGLE_FUNCTION_TARGET=testFunction",
+				"GOOGLE_RUNTIME=ruby26",
+			)
+
+			acceptance.TestBuildFailure(t, builder, tc)
+		})
+	}
+}
---
+++ cmd/ruby/functions_framework/main_test.go
@@ -0,0 +1,58 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"testing"
+
+	gcp "github.com/GoogleCloudPlatform/buildpacks/pkg/gcpbuildpack"
+)
+
+func TestDetect(t *testing.T) {
+	testCases := []struct {
+		name  string
+		files map[string]string
+		env   []string
+		stack string
+		want  int
+	}{
+		{
+			name: "with target",
+			env:  []string{"GOOGLE_FUNCTION_TARGET=helloWorld"},
+			want: 0,
+		},
+		{
+			name:  "with target GCF",
+			env:   []string{"FUNCTION_TARGET=helloWorld"},
+			stack: "google.ruby",
+			want:  0,
+		},
+		{
+			name:  "with target GCP",
+			env:   []string{"FUNCTION_TARGET=helloWorld"},
+			stack: "google",
+			want:  100,
+		},
+		{
+			name: "without target",
+			want: 100,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			gcp.TestDetectWithStack(t, detectFn, tc.name, tc.files, tc.env, tc.stack, tc.want)
+		})
+	}
+}
