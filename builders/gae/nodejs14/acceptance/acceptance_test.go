e58f411b9971370cc8fa8ae282497f516231bd44
Joel Gerard <joelgerard@google.com>
2020-07-20 11:00:02 -0700

    Re-enable buildpack acceptance tests.
    PiperOrigin-RevId: 322180609
    Change-Id: I6ea6f9b051840d17877fdd508430b83a7d1718bf

--- builders/gae/nodejs14/acceptance/acceptance_test.go
+++ builders/gae/nodejs14/acceptance/acceptance_test.go
@@ -32,145 +32,143 @@ const (
 )
 
 func TestAcceptance(t *testing.T) {
-	// todo(joelgerard): re-enable
-	t.Log("Pass")
-	// 	builder, cleanup := acceptance.CreateBuilder(t)
-	// 	t.Cleanup(cleanup)
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
 
-	// 	testCases := []acceptance.Test{
-	// 		{
-	// 			App:        "no_package_json",
-	// 			MustUse:    []string{appEngine},
-	// 			MustNotUse: []string{npm, yarn},
-	// 		},
-	// 		{
-	// 			App:        "package_json_no_dependencies",
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			App:        "package_json",
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			App:        "package_lock",
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			App:        "yarn_lock",
-	// 			MustUse:    []string{yarn},
-	// 			MustNotUse: []string{npm},
-	// 		},
-	// 		{
-	// 			App:        "yarn_lock_before_package_lock",
-	// 			MustUse:    []string{yarn},
-	// 			MustNotUse: []string{npm},
-	// 		},
-	// 		{
-	// 			App:        "postinstall_npm",
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{npmGCPBuild, yarn, yarnGCPBuild},
-	// 			MustNotOutputCached: []string{
-	// 				"removing existing node_modules/ before installation",
-	// 				"added 1 package",
-	// 			},
-	// 		},
-	// 		{
-	// 			App:              "postinstall_yarn",
-	// 			MustUse:          []string{yarn},
-	// 			MustNotUse:       []string{npmGCPBuild, npm, yarnGCPBuild},
-	// 			MustOutputCached: []string{"Already up-to-date."},
-	// 		},
-	// 		{
-	// 			App:        "gcp_build_npm",
-	// 			MustUse:    []string{npmGCPBuild},
-	// 			MustNotUse: []string{yarnGCPBuild},
-	// 		},
-	// 		{
-	// 			App:        "gcp_build_yarn",
-	// 			MustUse:    []string{yarnGCPBuild},
-	// 			MustNotUse: []string{npmGCPBuild},
-	// 		},
-	// 		{
-	// 			App:        "gcp_build_npm_no_dependencies",
-	// 			MustUse:    []string{npmGCPBuild},
-	// 			MustNotUse: []string{yarnGCPBuild},
-	// 		},
-	// 		{
-	// 			App:        "gcp_build_yarn_no_dependencies",
-	// 			MustUse:    []string{yarnGCPBuild},
-	// 			MustNotUse: []string{npmGCPBuild},
-	// 		},
-	// 		{
-	// 			App:        "local_dependency_npm",
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			App:        "local_dependency_yarn",
-	// 			MustUse:    []string{yarn},
-	// 			MustNotUse: []string{npm},
-	// 		},
-	// 		{
-	// 			App:        "dev_dependency_npm",
-	// 			Env:        []string{"NODE_ENV=development"},
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			App:        "dev_dependency_yarn",
-	// 			Env:        []string{"NODE_ENV=development"},
-	// 			MustUse:    []string{yarn},
-	// 			MustNotUse: []string{npm},
-	// 		},
-	// 		{
-	// 			App:        "node_modules_bin_npm",
-	// 			Path:       "/index.txt",
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			Name:       "node_modules/.bin custom entrypoint",
-	// 			App:        "node_modules_bin_npm",
-	// 			Path:       "/index.txt",
-	// 			Env:        []string{"GOOGLE_ENTRYPOINT=http-server -p $PORT"},
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 		{
-	// 			App:        "node_modules_bin_yarn",
-	// 			Path:       "/index.txt",
-	// 			MustUse:    []string{yarn},
-	// 			MustNotUse: []string{npm},
-	// 		},
-	// 		{
-	// 			Name:       "node_modules/.bin yarn custom entrypoint",
-	// 			App:        "node_modules_bin_yarn",
-	// 			Path:       "/index.txt",
-	// 			Env:        []string{"GOOGLE_ENTRYPOINT=http-server -p $PORT"},
-	// 			MustUse:    []string{yarn},
-	// 			MustNotUse: []string{npm},
-	// 		},
-	// 		{
-	// 			App:        "custom_entrypoint",
-	// 			Env:        []string{"GOOGLE_ENTRYPOINT=node custom.js"},
-	// 			MustUse:    []string{npm},
-	// 			MustNotUse: []string{yarn},
-	// 		},
-	// 	}
-	// 	for _, tc := range testCases {
-	// 		tc := tc
-	// 		if tc.Name == "" {
-	// 			tc.Name = tc.App
-	// 		}
-	// 		tc.Env = append(tc.Env, "GOOGLE_RUNTIME=nodejs14")
+	testCases := []acceptance.Test{
+		{
+			App:        "no_package_json",
+			MustUse:    []string{appEngine},
+			MustNotUse: []string{npm, yarn},
+		},
+		{
+			App:        "package_json_no_dependencies",
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			App:        "package_json",
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			App:        "package_lock",
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			App:        "yarn_lock",
+			MustUse:    []string{yarn},
+			MustNotUse: []string{npm},
+		},
+		{
+			App:        "yarn_lock_before_package_lock",
+			MustUse:    []string{yarn},
+			MustNotUse: []string{npm},
+		},
+		{
+			App:        "postinstall_npm",
+			MustUse:    []string{npm},
+			MustNotUse: []string{npmGCPBuild, yarn, yarnGCPBuild},
+			MustNotOutputCached: []string{
+				"removing existing node_modules/ before installation",
+				"added 1 package",
+			},
+		},
+		{
+			App:              "postinstall_yarn",
+			MustUse:          []string{yarn},
+			MustNotUse:       []string{npmGCPBuild, npm, yarnGCPBuild},
+			MustOutputCached: []string{"Already up-to-date."},
+		},
+		{
+			App:        "gcp_build_npm",
+			MustUse:    []string{npmGCPBuild},
+			MustNotUse: []string{yarnGCPBuild},
+		},
+		{
+			App:        "gcp_build_yarn",
+			MustUse:    []string{yarnGCPBuild},
+			MustNotUse: []string{npmGCPBuild},
+		},
+		{
+			App:        "gcp_build_npm_no_dependencies",
+			MustUse:    []string{npmGCPBuild},
+			MustNotUse: []string{yarnGCPBuild},
+		},
+		{
+			App:        "gcp_build_yarn_no_dependencies",
+			MustUse:    []string{yarnGCPBuild},
+			MustNotUse: []string{npmGCPBuild},
+		},
+		{
+			App:        "local_dependency_npm",
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			App:        "local_dependency_yarn",
+			MustUse:    []string{yarn},
+			MustNotUse: []string{npm},
+		},
+		{
+			App:        "dev_dependency_npm",
+			Env:        []string{"NODE_ENV=development"},
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			App:        "dev_dependency_yarn",
+			Env:        []string{"NODE_ENV=development"},
+			MustUse:    []string{yarn},
+			MustNotUse: []string{npm},
+		},
+		{
+			App:        "node_modules_bin_npm",
+			Path:       "/index.txt",
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			Name:       "node_modules/.bin custom entrypoint",
+			App:        "node_modules_bin_npm",
+			Path:       "/index.txt",
+			Env:        []string{"GOOGLE_ENTRYPOINT=http-server -p $PORT"},
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+		{
+			App:        "node_modules_bin_yarn",
+			Path:       "/index.txt",
+			MustUse:    []string{yarn},
+			MustNotUse: []string{npm},
+		},
+		{
+			Name:       "node_modules/.bin yarn custom entrypoint",
+			App:        "node_modules_bin_yarn",
+			Path:       "/index.txt",
+			Env:        []string{"GOOGLE_ENTRYPOINT=http-server -p $PORT"},
+			MustUse:    []string{yarn},
+			MustNotUse: []string{npm},
+		},
+		{
+			App:        "custom_entrypoint",
+			Env:        []string{"GOOGLE_ENTRYPOINT=node custom.js"},
+			MustUse:    []string{npm},
+			MustNotUse: []string{yarn},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		if tc.Name == "" {
+			tc.Name = tc.App
+		}
+		tc.Env = append(tc.Env, "GOOGLE_RUNTIME=nodejs14")
 
-	// 		t.Run(tc.Name, func(t *testing.T) {
-	// 			t.Parallel()
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
 
-	// 			acceptance.TestApp(t, builder, tc)
-	// 		})
-	// 	}
+			acceptance.TestApp(t, builder, tc)
+		})
+	}
 }
