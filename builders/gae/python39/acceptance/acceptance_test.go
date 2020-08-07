aa3d58d928e1630df2dd809aa81e3c1f5b4b9045
Joel Gerard <joelgerard@google.com>
2020-08-07 13:19:31 -0700

    Add the buildpack builder definition and acceptance tests for python39.
    PiperOrigin-RevId: 325495844
    Change-Id: I7f4b6a867c174b2438d84114a0be8db8765ae429

---
+++ builders/gae/python39/acceptance/acceptance_test.go
@@ -0,0 +1,73 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/buildpacks/pkg/acceptance"
+)
+
+func init() {
+	acceptance.DefineFlags()
+}
+
+func TestAcceptance(t *testing.T) {
+	// TODO(b/163086885): Remove after publishing images.
+	if acceptance.PullImages() {
+		t.Skip("Tests are skipped until stack images are published to gcr.io")
+	}
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.Test{
+		{
+			App: "no_requirements_txt",
+		},
+		{
+			App: "requirements_txt",
+		},
+		{
+			App: "pip_dependency",
+		},
+		{
+			App: "gunicorn_present",
+		},
+		{
+			App: "gunicorn_outdated",
+		},
+		{
+			App: "custom_entrypoint",
+			Env: []string{"GOOGLE_ENTRYPOINT=uwsgi --http :$PORT --wsgi-file custom.py --callable app"},
+		},
+		{
+			Name: "custom gunicorn entrypoint",
+			App:  "gunicorn_present",
+			Env:  []string{"GOOGLE_ENTRYPOINT=gunicorn main:app"},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		if tc.Name == "" {
+			tc.Name = tc.App
+		}
+		tc.Env = append(tc.Env, "GOOGLE_RUNTIME=python39")
+
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			acceptance.TestApp(t, builder, tc)
+		})
+	}
+}
