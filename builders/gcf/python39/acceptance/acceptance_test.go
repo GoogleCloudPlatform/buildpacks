4cd048138897355575e15bcf3a20dc3ae9497443
Michael Traver <mtraver@google.com>
2020-09-01 11:19:55 -0700

    Add buildpacks configuration for python39
    PiperOrigin-RevId: 329544703
    Change-Id: I9031c5b03915631a27e45b803327c3a439da1242

---
+++ builders/gcf/python39/acceptance/acceptance_test.go
@@ -0,0 +1,94 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/buildpacks/pkg/acceptance"
+)
+
+func init() {
+	acceptance.DefineFlags()
+}
+
+func TestAcceptance(t *testing.T) {
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.Test{
+		{
+			Name: "function without framework",
+			App:  "without_framework",
+		},
+		{
+			Name: "function with dependencies",
+			App:  "with_dependencies",
+		},
+		{
+			Name: "function with framework",
+			App:  "with_framework",
+		},
+		{
+			Name:   "function with runtime env var",
+			App:    "with_env_var",
+			RunEnv: []string{"FOO=foo"},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.Path = "/testFunction"
+			tc.Env = append(tc.Env,
+				"GOOGLE_FUNCTION_TARGET=testFunction",
+				"GOOGLE_RUNTIME=python39",
+			)
+			tc.FilesMustExist = append(tc.FilesMustExist,
+				"/layers/google.utils.archive-source/src/source-code.tar.gz",
+				"/workspace/.googlebuild/source-code.tar.gz",
+			)
+
+			acceptance.TestApp(t, builder, tc)
+		})
+	}
+}
+
+func TestFailures(t *testing.T) {
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.FailureTest{
+		{
+			App:       "fail_syntax_error",
+			Env:       []string{"GOOGLE_FUNCTION_TARGET=testFunction", "GOOGLE_RUNTIME=python39"},
+			MustMatch: "SyntaxError: invalid syntax",
+		},
+		{
+			App:       "fail_broken_dependencies",
+			Env:       []string{"GOOGLE_FUNCTION_TARGET=testFunction", "GOOGLE_RUNTIME=python39"},
+			MustMatch: `functions-framework .* has requirement flask<2\.0,>=1\.0, but you have flask 0\.12\.5`,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.App, func(t *testing.T) {
+			t.Parallel()
+
+			acceptance.TestBuildFailure(t, builder, tc)
+		})
+	}
+}
