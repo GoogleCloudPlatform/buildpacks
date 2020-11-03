97bbe235d46ed21e5ce56a40d09f82f1560070a0
Lukas Berger <bergerl@google.com>
2020-11-03 11:03:22 -0800

    Remove converter/with-framework from nodejs/functions_framework buildpack
    Having a package.json and a package-lock.json is unnecessary in this case, we can skip the installation altogether.
    
    PiperOrigin-RevId: 340481773
    Change-Id: Iad972c1ef8983e9c63879ece8bf19b7e2630fe54

--- cmd/nodejs/functions_framework/main.go
+++ cmd/nodejs/functions_framework/main.go
@@ -25,10 +25,12 @@ import (
 	"github.com/GoogleCloudPlatform/buildpacks/pkg/env"
 	gcp "github.com/GoogleCloudPlatform/buildpacks/pkg/gcpbuildpack"
 	"github.com/GoogleCloudPlatform/buildpacks/pkg/nodejs"
+	"github.com/buildpacks/libcnb"
 )
 
 const (
-	layerName = "functions-framework"
+	layerName                 = "functions-framework"
+	functionsFrameworkPackage = "@google-cloud/functions-framework"
 )
 
 func main() {
@@ -42,6 +44,11 @@ func detectFn(ctx *gcp.Context) (gcp.DetectResult, error) {
 	return gcp.OptOutEnvNotSet(env.FunctionTarget), nil
 }
 
+// buildFn sets up the execution environment for the function.
+// For a function that specifies the framework as a dependency, only set
+// environment variables and define a web process. The framework is
+// installed in the npm or yarn buildpack with other dependencies.
+// For a function that does not, also install the framework.
 func buildFn(ctx *gcp.Context) error {
 	if _, ok := os.LookupEnv(env.FunctionSource); ok {
 		return gcp.UserErrorf("%s is not currently supported for Node.js buildpacks", env.FunctionSource)
@@ -61,7 +68,7 @@ func buildFn(ctx *gcp.Context) error {
 		if err != nil {
 			return fmt.Errorf("reading package.json: %w", err)
 		}
-		_, hasFrameworkDependency = pjs.Dependencies["@google-cloud/functions-framework"]
+		_, hasFrameworkDependency = pjs.Dependencies[functionsFrameworkPackage]
 		if pjs.Main != "" {
 			fnFile = pjs.Main
 		}
@@ -71,21 +78,48 @@ func buildFn(ctx *gcp.Context) error {
 		return gcp.UserErrorf("%s does not exist", fnFile)
 	}
 
-	// Syntax check the function code without executing.
+	// Syntax check the function code without executing to prevent run-time errors.
 	ctx.Exec([]string{"node", "--check", fnFile}, gcp.WithUserAttribution)
 
-	cvt := filepath.Join(ctx.BuildpackRoot(), "converter")
+	l := ctx.Layer(layerName, gcp.BuildLayer, gcp.CacheLayer, gcp.LaunchLayer)
+	// We use the absolute path to the functions-framework executable in order to
+	// avoid having to add its parent directory to PATH which could cause
+	// conflicts with user-specified dependencies in the case where the framework
+	// is not an explicit dependency.
+	//
+	// If the function specifies a framework dependency, the executable will be
+	// in node_modules, where it would have been installed by the preceding
+	// npm/yarn buildpack. Otherwise, it will be in the layer's node_modules,
+	// installed below.
+	ff := filepath.Join(".bin", "functions-framework")
+
 	if hasFrameworkDependency {
 		ctx.Logf("Handling functions with dependency on functions-framework.")
-		cvt = filepath.Join(cvt, "with-framework")
+		ctx.ClearLayer(l)
+		ff = filepath.Join("node_modules", ff)
 	} else {
 		ctx.Logf("Handling functions without dependency on functions-framework.")
-		cvt = filepath.Join(cvt, "without-framework")
+
+		if err := installFunctionsFramework(ctx, l); err != nil {
+			return fmt.Errorf("installing funtions-framework: %w", err)
+		}
+
+		ff = filepath.Join(l.Path, "node_modules", ff)
+
+		// Add user's node_modules to NODE_PATH so functions-framework can always find user's packages.
+		if nm := filepath.Join(ctx.ApplicationRoot(), "node_modules"); ctx.FileExists(nm) {
+			l.LaunchEnvironment.PrependPath("NODE_PATH", nm)
+		}
 	}
 
-	// Install functions-framework.
-	l := ctx.Layer(layerName, gcp.BuildLayer, gcp.CacheLayer, gcp.LaunchLayer)
-	nm := filepath.Join(l.Path, "node_modules")
+	ctx.SetFunctionsEnvVars(l)
+	ctx.AddWebProcess([]string{"/bin/bash", "-c", ff})
+	return nil
+}
+
+// installFunctionsFramework downloads the functions-framework package to node_modules in the given layer.
+func installFunctionsFramework(ctx *gcp.Context, l *libcnb.Layer) error {
+	cvt := filepath.Join(ctx.BuildpackRoot(), "converter", "without-framework")
 	pjs := filepath.Join(cvt, "package.json")
 	pljs := filepath.Join(cvt, nodejs.PackageLock)
 
@@ -95,32 +129,13 @@ func buildFn(ctx *gcp.Context) error {
 	}
 	if cached {
 		ctx.CacheHit(layerName)
-	} else {
-		ctx.CacheMiss(layerName)
-		ctx.ClearLayer(l)
-		// NPM expects package.json and the lock file in the prefix directory.
-		ctx.Exec([]string{"cp", "-t", l.Path, pjs, pljs}, gcp.WithUserTimingAttribution)
-		ctx.Exec([]string{"npm", nodejs.NPMInstallCommand(ctx), "--quiet", "--production", "--prefix", l.Path}, gcp.WithUserAttribution)
+		return nil
 	}
 
-	// Determine the path to the executable file to start functions-framework.
-	// If the function has dependency on functions-framework, it should already
-	// be installed in node_modules.
-	// Else, it is installed in functions-framework layer's node_modules.
-	ff := filepath.Join(".bin", "functions-framework")
-	if hasFrameworkDependency {
-		ff = filepath.Join("node_modules", ff)
-	} else {
-		ff = filepath.Join(nm, ff)
-
-		// Add user's node_modules to NODE_PATH so functions-framework can always find user's packages.
-		unm := filepath.Join(ctx.ApplicationRoot(), "node_modules")
-		if ctx.FileExists(unm) {
-			l.LaunchEnvironment.PrependPath("NODE_PATH", unm)
-		}
-	}
-
-	ctx.SetFunctionsEnvVars(l)
-	ctx.AddWebProcess([]string{"/bin/bash", "-c", ff})
+	ctx.CacheMiss(layerName)
+	ctx.ClearLayer(l)
+	// NPM expects package.json and the lock file in the prefix directory.
+	ctx.Exec([]string{"cp", "-t", l.Path, pjs, pljs}, gcp.WithUserTimingAttribution)
+	ctx.Exec([]string{"npm", nodejs.NPMInstallCommand(ctx), "--quiet", "--production", "--prefix", l.Path}, gcp.WithUserAttribution)
 	return nil
 }
