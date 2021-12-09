a1017b9d48e4537a6cde51db4958ba8b51a06d05
GCP Buildpacks Team <noreply@google.com>
2021-12-09 11:58:07 -0800

    Add buildpacks configuration for GCF python310
    PiperOrigin-RevId: 415320347
    Change-Id: I5b0dedae9dca7547c012b2c21d7a3639177034a8

---
+++ builders/gcf/python310/acceptance/acceptance_test.go
@@ -0,0 +1,120 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/buildpacks/internal/acceptance"
+)
+
+func init() {
+	acceptance.DefineFlags()
+}
+
+func TestAcceptance(t *testing.T) {
+	// TODO(b/193267779): Remove when stack images are published.
+	if acceptance.PullImages() {
+		t.Skip("Disabled for continuous builds")
+	}
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.Test{
+		{
+			Name:          "function without framework",
+			App:           "without_framework",
+			MustNotOutput: []string{`WARNING: You are using pip version`},
+		},
+		{
+			Name: "function with dependencies",
+			App:  "with_dependencies",
+		},
+		{
+			Name: "function with framework",
+			App:  "with_framework",
+		},
+		{
+			Name:          "function using http declarative function signatures",
+			App:           "use_declarative",
+			MustNotOutput: []string{"WARNING"},
+		},
+		{
+			Name:          "function using CloudEvent declarative function signatures",
+			App:           "use_cloud_event_declarative",
+			MustMatch:     "OK",
+			MustNotOutput: []string{"WARNING"},
+			RequestType:   acceptance.CloudEventType,
+			SkipCacheTest: true,
+		},
+		{
+			Name: "function with framework and dependency bin",
+			App:  "with_framework_bin_conflict",
+		},
+		{
+			Name:   "function with runtime env var",
+			App:    "with_env_var",
+			RunEnv: []string{"FOO=foo"},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.Path = "/testFunction"
+			tc.Env = append(tc.Env,
+				"GOOGLE_FUNCTION_TARGET=testFunction",
+				"GOOGLE_RUNTIME=python310",
+			)
+			tc.FilesMustExist = append(tc.FilesMustExist,
+				"/layers/google.utils.archive-source/src/source-code.tar.gz",
+				"/workspace/.googlebuild/source-code.tar.gz",
+			)
+
+			acceptance.TestApp(t, builder, tc)
+		})
+	}
+}
+
+func TestFailures(t *testing.T) {
+	// TODO(b/193267779): Remove when stack images are published.
+	if acceptance.PullImages() {
+		t.Skip("Disabled for continuous builds")
+	}
+	builder, cleanup := acceptance.CreateBuilder(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.FailureTest{
+		{
+			App:       "fail_syntax_error",
+			Env:       []string{"GOOGLE_FUNCTION_TARGET=testFunction", "GOOGLE_RUNTIME=python310"},
+			MustMatch: "SyntaxError: invalid syntax",
+		},
+		{
+			App:       "fail_broken_dependencies",
+			Env:       []string{"GOOGLE_FUNCTION_TARGET=testFunction", "GOOGLE_RUNTIME=python310"},
+			MustMatch: `functions-framework .* has requirement flask<3\.0,>=1\.0, but you have flask 0\.12\.5`,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.App, func(t *testing.T) {
+			t.Parallel()
+
+			acceptance.TestBuildFailure(t, builder, tc)
+		})
+	}
+}
