03c47280e39531188d645f03dd7e22620fe99372
Matthew Robertson <mattrobertson@google.com>
2022-01-14 11:25:43 -0800

    Create a dart/compile buildpack
    This commit adds a buildpack for compiling a dart application into a
    self-contained executable. For now the buildpack is not used. I plan to
    enable it in the GCP builder in a subsequent commit along with other
    dart specific buildpacks.
    
    PiperOrigin-RevId: 421873087
    Change-Id: Iadc96ac3387f24d95ef96f930a700c678d189647

---
+++ cmd/dart/compile/main.go
@@ -0,0 +1,68 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Implements dart/compile buildpack.
+// The compile buildpack runs dart compile to produce a self-contained executable.
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/GoogleCloudPlatform/buildpacks/pkg/env"
+	gcp "github.com/GoogleCloudPlatform/buildpacks/pkg/gcpbuildpack"
+)
+
+func main() {
+	gcp.Main(detectFn, buildFn)
+}
+
+func detectFn(ctx *gcp.Context) (gcp.DetectResult, error) {
+	if !ctx.HasAtLeastOne("*.dart") {
+		return gcp.OptOut("no .dart files found"), nil
+	}
+	return gcp.OptIn("found .dart files"), nil
+}
+
+func buildFn(ctx *gcp.Context) error {
+	// Create a layer for the compiled binary.  Add it to PATH in case
+	// users wish to invoke the binary manually.
+	bl := ctx.Layer("bin", gcp.LaunchLayer)
+	bl.LaunchEnvironment.Prepend("PATH", string(os.PathListSeparator), bl.Path)
+	outBin := filepath.Join(bl.Path, "main")
+
+	buildable, err := dartBuildable(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to find a valid buildable: %w", err)
+	}
+
+	// Build the application.
+	bld := []string{"dart", "compile", "exe", buildable, "-o", outBin}
+	ctx.Exec(bld, gcp.WithUserAttribution)
+
+	ctx.AddWebProcess([]string{outBin})
+	return nil
+}
+
+func dartBuildable(ctx *gcp.Context) (string, error) {
+
+	// The user tells us what to build.
+	if buildable, ok := os.LookupEnv(env.Buildable); ok {
+		return buildable, nil
+	}
+
+	// Default to main.dart in the application root.
+	return "main.dart", nil
+}
