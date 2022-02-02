647850eeea968f2a3926903a0e9466bf42d57428
Matthew Robertson <mattrobertson@google.com>
2022-02-02 14:19:32 -0800

    add utils for resolving semantic versions
    These will be used for installing the correct version of a runtime (or other tool) based on the a version specifier and a manifest of available versions.
    
    PiperOrigin-RevId: 425976632
    Change-Id: Ieaecfd68a620773ed66fce2ca59831e29119d7bb

---
+++ pkg/version/version.go
@@ -0,0 +1,58 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package version provides utility methods for working with semantic versions.
+package version
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/Masterminds/semver"
+)
+
+// ResolveVersion finds the largest version in a list of semantic versions that satisifies the
+// provided constraint. If no version in the list statisfies the constraint it returns an error.
+func ResolveVersion(constraint string, versions []string) (string, error) {
+	c, err := semver.NewConstraint(constraint)
+	if err != nil {
+		return "", err
+	}
+
+	semvers := make([]*semver.Version, len(versions))
+	for i, version := range versions {
+		s, err := semver.NewVersion(version)
+		if err != nil {
+			return "", err
+		}
+		semvers[i] = s
+	}
+
+	// Sort in descending order so that the first version in the list to satisify a constraint will be
+	// the highest possible version.
+	sort.Sort(sort.Reverse(semver.Collection(semvers)))
+	for _, s := range semvers {
+		if c.Check(s) {
+			return s.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("failed to resolve version matching: %v", c)
+}
+
+// IsExactSemver returns true if a given string is valid semantic version.
+func IsExactSemver(constraint string) bool {
+	_, err := semver.NewVersion(constraint)
+	return err == nil
+}
---
+++ pkg/version/version_test.go
@@ -0,0 +1,119 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package version
+
+import (
+	"testing"
+)
+
+func TestResolveVersion(t *testing.T) {
+	testCases := []struct {
+		name       string
+		constraint string
+		versions   []string
+		want       string
+		wantError  bool
+	}{
+		{
+			name:       "tilda specifier",
+			constraint: "~1.2.1",
+			versions:   []string{"1.2.3", "1.2.4", "1.3.0", "0.1.2", "2.0.0"},
+			want:       "1.2.4",
+		},
+		{
+			name:       "carat specifier",
+			constraint: "^1.2.1",
+			versions:   []string{"1.2.3", "1.2.4", "1.3.0", "0.1.2", "2.0.0"},
+			want:       "1.3.0",
+		},
+		{
+			name:       "complex constraint",
+			constraint: ">= 1.2.3, < 1.2.4",
+			versions:   []string{"1.2.3", "1.2.4", "1.3.0", "0.1.2", "2.0.0"},
+			want:       "1.2.3",
+		},
+		{
+			name:       "strips prefix",
+			constraint: "v10.1.1",
+			versions:   []string{"v10.1.1"},
+			want:       "10.1.1",
+		},
+		{
+			name:       "invalid constraint",
+			constraint: "xyz",
+			versions:   []string{"v10.1.1"},
+			wantError:  true,
+		},
+		{
+			name:       "no matching version",
+			constraint: ">=2.0.0",
+			versions:   []string{"1.2.3", "1.2.4"},
+			wantError:  true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := ResolveVersion(tc.constraint, tc.versions)
+			if tc.wantError != (err != nil) {
+				t.Errorf("ResolveVersion(%q, %v) got error: %v, want error?: %v", tc.constraint, tc.versions, err, tc.wantError)
+			}
+			if got != tc.want {
+				t.Errorf("ResolveVersion(%q, %v) = %q, want %q", tc.constraint, tc.versions, got, tc.want)
+			}
+		})
+	}
+}
+
+func TestIsExactSemver(t *testing.T) {
+	testCases := []struct {
+		version string
+		want    bool
+	}{
+		{
+			version: "v10.1.1",
+			want:    true,
+		},
+		{
+			version: "1.1",
+			want:    true,
+		},
+		{
+			version: "",
+			want:    false,
+		},
+		{
+			version: "1.x.x",
+			want:    false,
+		},
+		{
+			version: "~1.0.0",
+			want:    false,
+		},
+		{
+			version: ">=1.0.0",
+			want:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.version, func(t *testing.T) {
+			got := IsExactSemver(tc.version)
+			if got != tc.want {
+				t.Errorf("IsExactSemver(%q) = %t, want %t", tc.version, got, tc.want)
+			}
+		})
+	}
+}
