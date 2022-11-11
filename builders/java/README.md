e2c72b584c5dbaf217ab8c12bd3dac4953250988
Paul Feng <paulfeng@google.com>
2022-11-11 15:42:48 -0800

    Implement the java unified builder with support for GCP
    Support for GCF and GAE will come in a subsequent commit.
    
    PiperOrigin-RevId: 487934341
    Change-Id: Ic8353e6a4438313e5986e9937df141d9ac153cf2

---
+++ builders/java/README.md
@@ -0,0 +1,33 @@
+# The Java Builder
+This directory contains the definition of the Java builder.
+
+## Build the Image
+To build the builder image, run:
+
+```bash
+bazel build //builders/java:builder.image
+```
+
+## Build a Test Application
+To build the sample application [gomod_go_sum](../testdata/java/), run:
+
+```bash
+pack build sample-java --builder gcp/java --path <path to java app> --trust-builder -v
+```
+
+## Acceptance Tests
+To run the acceptance tests across all the products, run:
+
+```bash
+bazel test //builders/java/acceptance:acceptance_test
+```
+
+### Test a Single Product
+For each product, there exists a suite of tests. The build target is
+`builders/go/acceptance:<product>_test`. Where `<product>` is replaced with the
+acronym of the product. For example, to run the tests for Google Cloud
+Functions, run:
+
+```bash
+bazel test //builders/java/acceptance:gcf_test
+```
---
+++ builders/java/acceptance/gcp_test.go
@@ -0,0 +1,216 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package acceptance_test
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/buildpacks/internal/acceptance"
+)
+
+const (
+	// Buildpack identifiers used to verify that buildpacks were or were not used.
+	entrypoint      = "google.config.entrypoint"
+	javaClearSource = "google.java.clear_source"
+	javaEntrypoint  = "google.java.entrypoint"
+	javaExplodedJar = "google.java.exploded-jar"
+	javaGradle      = "google.java.gradle"
+	javaMaven       = "google.java.maven"
+	javaRuntime     = "google.java.runtime"
+)
+
+func init() {
+	acceptance.DefineFlags()
+}
+
+func TestAcceptanceJava(t *testing.T) {
+	imageContext, cleanup := acceptance.ProvisionImages(t)
+	t.Cleanup(cleanup)
+
+	testCases := []acceptance.Test{
+		{
+			Name: "simple Java 11 application",
+			App:  "simple",
+			Env: []string{
+				"GOOGLE_ENTRYPOINT=java Main.java",
+				"GOOGLE_RUNTIME_VERSION=11",
+			},
+			MustUse:         []string{javaRuntime, entrypoint},
+			MustNotUse:      []string{javaEntrypoint},
+			EnableCacheTest: true,
+		},
+		{
+			Name: "simple Java 17 application",
+			App:  "simple",
+			Env: []string{
+				"GOOGLE_ENTRYPOINT=java Main.java",
+				"GOOGLE_RUNTIME_VERSION=17",
+			},
+			MustUse:         []string{javaRuntime, entrypoint},
+			MustNotUse:      []string{javaEntrypoint},
+			EnableCacheTest: true,
+		},
+		{
+			Name: "Java runtime version respected",
+			App:  "simple",
+			// Checking runtime version 8 to ensure that it is not downloading latest Java 11 version.
+			Path: "/version?want=8",
+			Env: []string{
+				"GOOGLE_ENTRYPOINT=javac Main.java; java Main",
+				"GOOGLE_RUNTIME_VERSION=8",
+			},
+			MustUse:    []string{javaRuntime, entrypoint},
+			MustNotUse: []string{javaEntrypoint},
+		},
+		{
+			Name:            "Java maven",
+			App:             "hello_quarkus_maven",
+			MustUse:         []string{javaMaven, javaRuntime, javaEntrypoint},
+			MustNotUse:      []string{entrypoint},
+			EnableCacheTest: true,
+		},
+		{
+			Name:                "Java maven (Dev Mode)",
+			App:                 "hello_quarkus_maven",
+			Env:                 []string{"GOOGLE_DEVMODE=1"},
+			FilesMustExist:      []string{"/layers/google.java.maven/m2", "/layers/google.java.maven/m2/bin/.devmode_rebuild.sh"},
+			MustRebuildOnChange: "/workspace/src/main/java/hello/Hello.java",
+		},
+		// TODO(b/189485322): Enable the below test when exact version can be pinned.
+		// {
+		// 	// This is a separate test case from Dev mode above because it has a fixed runtime version.
+		// 	// Its only purpose is to test that the metadata is set correctly.
+		// 	Name: "Dev mode metadata",
+		// 	App:  "hello_quarkus_maven",
+		// 	Env:  []string{"GOOGLE_DEVMODE=1", "GOOGLE_RUNTIME_VERSION=11"},
+		// 	BOM: []acceptance.BOMEntry{
+		// 		{
+		// 			Name: "java",
+		// 			Metadata: map[string]interface{}{
+		// 				"version": "11",
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: "devmode",
+		// 			Metadata: map[string]interface{}{
+		// 				"devmode.sync": []interface{}{
+		// 					map[string]interface{}{"dest": "/workspace", "src": "**/*.java"},
+		// 					map[string]interface{}{"dest": "/workspace", "src": "**/*.kt"},
+		// 					map[string]interface{}{"dest": "/workspace", "src": "**/*.scala"},
+		// 					map[string]interface{}{"dest": "/workspace", "src": "**/*.groovy"},
+		// 					map[string]interface{}{"dest": "/workspace", "src": "**/*.clj"},
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		{
+			Name:       "Java gradle",
+			App:        "gradle_micronaut",
+			Env:        []string{"GOOGLE_ENTRYPOINT=java -jar build/libs/helloworld-0.1-all.jar"},
+			MustUse:    []string{javaGradle, javaRuntime, entrypoint},
+			MustNotUse: []string{javaEntrypoint},
+		},
+		{
+			Name:                "Java gradle (Dev Mode)",
+			App:                 "gradle_micronaut",
+			Env:                 []string{"GOOGLE_DEVMODE=1"},
+			FilesMustExist:      []string{"/layers/google.java.gradle/cache", "/layers/google.java.gradle/cache/bin/.devmode_rebuild.sh"},
+			MustRebuildOnChange: "/workspace/src/main/java/example/HelloController.java",
+		},
+		{
+			Name: "polyglot maven java 11",
+			App:  "polyglot-maven",
+			Env: []string{
+				"GOOGLE_RUNTIME_VERSION=11",
+			},
+			MustUse:    []string{javaMaven, javaRuntime, javaEntrypoint},
+			MustNotUse: []string{entrypoint},
+		},
+		{
+			Name: "polyglot maven java 17",
+			App:  "polyglot-maven",
+			Env: []string{
+				"GOOGLE_RUNTIME_VERSION=17",
+			},
+			MustUse:    []string{javaMaven, javaRuntime, javaEntrypoint},
+			MustNotUse: []string{entrypoint},
+		},
+		{
+			Name:       "Maven build args",
+			App:        "maven_testing_profile",
+			Env:        []string{"GOOGLE_BUILD_ARGS=--settings=maven_settings.xml -Dnative=false"},
+			MustUse:    []string{javaMaven, javaRuntime, javaEntrypoint},
+			MustNotUse: []string{entrypoint},
+		},
+		{
+			Name:       "Gradle build args",
+			App:        "gradle_test_env",
+			Env:        []string{"GOOGLE_BUILD_ARGS=-Denv=test", "GOOGLE_ENTRYPOINT=java -jar build/libs/helloworld-0.1-all.jar"},
+			MustUse:    []string{javaGradle, javaRuntime, entrypoint},
+			MustNotUse: []string{javaEntrypoint},
+		},
+		{
+			Name: "Exploded Jar java 11",
+			App:  "exploded_jar",
+			Env: []string{
+				"GOOGLE_RUNTIME_VERSION=11",
+			},
+			MustUse: []string{javaRuntime, javaExplodedJar},
+		},
+		{
+			Name: "Exploded Jar java 17",
+			App:  "exploded_jar",
+			Env: []string{
+				"GOOGLE_RUNTIME_VERSION=17",
+			},
+			MustUse: []string{javaRuntime, javaExplodedJar},
+		},
+		{
+			Name:              "Maven with source clearing",
+			App:               "hello_quarkus_maven",
+			Env:               []string{"GOOGLE_CLEAR_SOURCE=true"},
+			MustUse:           []string{javaMaven, javaRuntime, javaEntrypoint, javaClearSource},
+			MustNotUse:        []string{entrypoint},
+			FilesMustExist:    []string{"/workspace/target/hello-1-runner.jar"},
+			FilesMustNotExist: []string{"/workspace/src/main/java/hello/Hello.java", "/workspace/pom.xml"},
+		},
+		{
+			Name:              "Gradle with source clearing",
+			App:               "gradle_micronaut",
+			Env:               []string{"GOOGLE_CLEAR_SOURCE=true", "GOOGLE_ENTRYPOINT=java -jar build/libs/helloworld-0.1-all.jar"},
+			MustUse:           []string{javaGradle, javaRuntime, entrypoint, javaClearSource},
+			MustNotUse:        []string{javaEntrypoint},
+			FilesMustExist:    []string{"/workspace/build/libs/helloworld-0.1-all.jar"},
+			FilesMustNotExist: []string{"/workspace/src/main/java/example/Application.java", "/workspace/build.gradle"},
+		},
+		{
+			Name:       "Multi-module",
+			App:        "multi_module",
+			Env:        []string{"GOOGLE_BUILDABLE=hello"},
+			MustUse:    []string{javaMaven, javaRuntime, javaEntrypoint},
+			MustNotUse: []string{entrypoint},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		tc.FlakyBuildAttempts = 3
+
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			acceptance.TestApp(t, imageContext, tc)
+		})
+	}
+}
