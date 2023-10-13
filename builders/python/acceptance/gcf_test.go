d850e76f019b2d2aa616651d4fdf78e5aad35333
Sriram Mahavadi <srirammahavadi@google.com>
2023-10-12 18:43:51 -0700

    Improving reliability of python acceptance tests
    PiperOrigin-RevId: 573069414
    Change-Id: If81973acfb23aad1f12e59c44ccff8de99cfc87b

--- builders/python/acceptance/gcf_test.go
+++ builders/python/acceptance/gcf_test.go
@@ -78,7 +78,8 @@ func TestAcceptance(t *testing.T) {
 	for _, tc := range testCases {
 		tc := applyStaticTestOptions(tc)
 		t.Run(tc.Name, func(t *testing.T) {
-			t.Parallel()
+			// Running these tests in parallel causes the server to run out of disk space.
+			// t.Parallel()
 			acceptance.TestApp(t, imageCtx, tc)
 		})
 	}
@@ -129,7 +130,8 @@ func TestFailures(t *testing.T) {
 	for _, tc := range acceptance.FilterFailureTests(t, testCases) {
 		tc := applyStaticFailureTestOptions(tc)
 		t.Run(tc.App, func(t *testing.T) {
-			t.Parallel()
+			// Running these tests in parallel causes the server to run out of disk space.
+			// t.Parallel()
 			acceptance.TestBuildFailure(t, imageCtx, tc)
 		})
 	}
