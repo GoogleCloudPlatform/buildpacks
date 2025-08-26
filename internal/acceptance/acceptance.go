13dd8bbeae443dfea99c28d954b59f0e059e272b
Srinjoy Ray <srinjoyray@google.com>
2025-08-26 07:52:16 -0700

    Internal change
    PiperOrigin-RevId: 799562571
    Change-Id: I0e725679e3347392da16b7cddcd65f2d0dbe948f

--- internal/acceptance/acceptance.go
+++ internal/acceptance/acceptance.go
@@ -934,6 +934,7 @@ func buildCommand(srcDir, image, builderName, runName string, env map[string]str
 	// value ensures that the generated builder sha is unique and removing it after one build will
 	// not affect other builds running concurrently.
 	args = append(args, "--env", "GOOGLE_RANDOM="+randString(8), "--env", "GOOGLE_DEBUG=true")
+	args = append(args, "--network", "host")
 	args = append(args, "--env", "GOOGLE_RUNTIME_IMAGE_REGION=us")
 	if buildEnv != "" {
 		args = append(args, "--env", fmt.Sprintf("GOOGLE_BUILD_ENV=%s", buildEnv))
