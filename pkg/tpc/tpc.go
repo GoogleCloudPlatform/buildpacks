8205426f3a562c669b3a3405108265e9dbc28926
Hemant Goyal <hemantgoyal@google.com>
2026-05-22 12:45:20 -0700

    INTERNAL CHANGE
    PiperOrigin-RevId: 919812491
    Change-Id: I200cc56d97610b6274bd0821f7a8e2bb044078b8

--- pkg/tpc/tpc.go
+++ pkg/tpc/tpc.go
@@ -7,40 +7,10 @@ import (
 	"github.com/GoogleCloudPlatform/buildpacks/pkg/env"
 )
 
-var (
-	universeToProject = map[string]string{
-		"prp": "tpczero-system/serverless-runtimes-tpc",
-		"tsp": "eu0-system/serverless-runtimes-tpc",
-		"tsq": "tpcone-system/serverless-runtimes-tpc",
-		"thp": "s3ns-system/serverless-runtimes-tpc",
-		"thq": "q-s3ns-system/serverless-runtimes-tpc",
-	}
-
-	arRegionToHostname = map[string]string{
-		"u-us-prp1":             "docker.pkg-tpczero.goog",
-		"u-germany-northeast1q": "docker.pkg-tpcone.goog",
-		"u-germany-northeast1":  "docker.pkg-berlin-build0.goog",
-		"u-france-east1":        "docker.s3nsregistry.fr",
-		"u-france-east1q":       "docker.qs3nsregistry.fr",
-	}
-)
-
 // IsTPC returns true if the build universe is set and is not GDU.
 func IsTPC() bool {
 	_, present := GetTarballProject()
 	return present
-}
-
-// ARRegionToHostname returns the Artifact Registry hostname for a given region in TPC.
-func ARRegionToHostname(region string) (string, bool) {
-	hostname, present := arRegionToHostname[region]
-	return hostname, present
-}
-
-// UniverseToProject returns the Artifact Registry project for a given universe.
-func UniverseToProject(universe string) (string, bool) {
-	project, present := universeToProject[universe]
-	return project, present
 }
 
 // GetTarballProject returns the Artifact Registry project for the TPC tarball.
